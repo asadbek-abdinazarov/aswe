520f2ea4386d261304e3e9dde3f808d58d0ef1f3
Asadbek <asadbekabdunazarov602@gmail.com>
2025-07-12 12:11:02 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -10,11 +10,16 @@ const nextConfig = {
     unoptimized: true,
   },
   allowedDevOrigins: [
-    'http://localhost:3000', // local host uchun
-    'http://172.20.10.4:3000', // sizning IP-manzilingiz
+    'http://localhost:3000',
+    'http://172.20.10.4:3000',
     'http://local-origin.dev',
     'http://64.226.76.213:3000',
+    'http://64.226.76.213:*',
+    'https://64.226.76.213:3000',
+    'https://64.226.76.213:*',
     'http://*.local-origin.dev',
+    'http://64.226.76.213',
+    'https://64.226.76.213',
   ],
 }
 
